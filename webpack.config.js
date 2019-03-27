44cb3cc36278a972d11e5e6c95bcf06ed6faa89f
nick-t-go <nickt444@gmail.com>
2019-03-27 18:52:35 -0400

    added a html load pkg

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
@@ -23,8 +24,19 @@ module.exports = {
           presets: ['@babel/preset-env'],
         },
       },
-    }],
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+        },
+      }],
+    },
+    ],
   },
+
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -41,18 +53,19 @@ module.exports = {
   },
 
   plugins: [
-    new CopyWebpackPlugin([{
-      from: path.resolve(__dirname, 'index.html'),
-      to: path.resolve(__dirname, 'build'),
-    },
-    {
-      from: path.resolve(__dirname, 'assets', '**', '*'),
-      to: path.resolve(__dirname, 'build'),
-    },
-    ]),
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
       'typeof WEBGL_RENDERER': JSON.stringify(true),
     }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'build'),
+      },
+    ]),
   ],
 };
